ec81859f048089b54adce80b6faf1c19d5d598f3
jaha01 <anvarov94@gmail.com>
2025-03-05 23:50:11 +0500

    Initial Commit

---
+++ cinemaHall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinemaHall
+//
+//  Created by Jahongir Anvarov on 05.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
